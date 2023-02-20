7ca34c0eaddd0142217f751a24112b91311588c3
Ritika Zhaira Mishra <116043234+ritikazahira@users.noreply.github.com>
2023-02-19 23:05:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def longest_run(mylist, key):
             current_run_length = 0
     return longest_run_length
     pass
-
+# Work and span for longest_run is O(n). 
 
 class Result:
     """ done """
@@ -36,8 +36,37 @@ class Result:
     
     
 def longest_run_recursive(mylist, key):
-    ### TODO
+    n = len(mylist)
+    if n == 0:
+        return Result(0, 0, 0, False)
+    elif n == 1:
+        if mylist[0] == key:
+            return Result(1, 1, 1, True)
+        else:
+            return Result(0, 0, 0, False)
+    else:
+        mid = n // 2
+        left_result = longest_run_recursive(mylist[:mid], key)
+        right_result = longest_run_recursive(mylist[mid:], key)
+        left_size, right_size, longest_size = left_result.left_size, right_result.right_size, 0
+        is_entire_range = False
+        if left_result.is_entire_range and right_result.is_entire_range and mylist[mid-1] == key and mylist[mid] == key:
+            is_entire_range = True
+            longest_size = left_result.right_size + right_result.left_size
+        elif left_result.is_entire_range:
+            if mylist[mid-1] == key:
+                left_size += right_result.left_size
+            longest_size = max(left_result.longest_size, right_result.longest_size, left_size)
+        elif right_result.is_entire_range:
+            if mylist[mid] == key:
+                right_size += left_result.right_size
+            longest_size = max(left_result.longest_size, right_result.longest_size, right_size)
+        else:
+            longest_size = max(left_result.longest_size, right_result.longest_size, left_size, right_size)
+        return Result(left_size, right_size, longest_size, is_entire_range)
     pass
+#3d. The work is W(n) = 2W(n/2)+1 so O(n) and the span is also O(n) since its sequential. 
+#3e. THe work is still O(n) and the span is O(log n) since its parallel. 
 
 ## Feel free to add your own tests here.
 def test_longest_run():
